74e7a6d21769ef809fb5bd27d21f9185d9f6a566
Hien Dao <hienqdao5@gmail.com>
2026-02-16 18:14:13 -0500

    Replace starter code

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,17 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stateful Lab',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      // Application name
+      title: 'Stateful Widget',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget that will be started on the application startup
       home: CounterWidget(),
     );
   }
@@ -21,35 +26,39 @@ class CounterWidget extends StatefulWidget {
 }
 
 class _CounterWidgetState extends State<CounterWidget> {
-  int _counter = 0; // This is our STATE
-
+  //initial counter value
+  int _counter = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Interactive Counter')),
+      appBar: AppBar(
+        title: const Text('Stateful Widget'),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
             child: Container(
-              color: Colors.blue.shade100,
-              padding: EdgeInsets.all(20),
+              color: Colors.blue,
               child: Text(
+                //displays the current number
                 '$_counter',
                 style: TextStyle(fontSize: 50.0),
               ),
             ),
           ),
-          SizedBox(height: 20),
           Slider(
-            min: 0, max: 100,
+            min: 0,
+            max: 100,
             value: _counter.toDouble(),
             onChanged: (double value) {
-              // 👇 This triggers the UI rebuild
               setState(() {
                 _counter = value.toInt();
               });
             },
+            activeColor: Colors.blue,
+            inactiveColor: Colors.red,
           ),
         ],
       ),
